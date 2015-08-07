e5a5372451796bded240ec9960574ee87b675890
Francois Maillet <francois@recoset.com>
2015-08-07 12:32:16 -0400

    Initial rebalancer commit

---
+++ rebalancer.py
@@ -0,0 +1,78 @@
+
+import csv
+import argparse
+
+def show_positions(args):
+    # Load symbols map
+    symbolsmap_rdr = csv.DictReader(open(args.symbolmap))
+    symbol_map = {}
+    for symbol_idx, symbol in enumerate(symbolsmap_rdr):
+        symbol_map[symbol['Stock symbol']] = symbol['Target symbol']
+
+    # Load allocation targets
+    targets_rdr = csv.DictReader(open(args.targets))
+    targets = {}
+    targets["Other"] = {"Name":"Other","Symbol":"Other","Target":0}
+    for target_idx, target in enumerate(targets_rdr):
+        if len(target) == 0: continue
+        targets[target['Symbol']] = target
+
+    # Load positions and group them by asset type
+    positions_rdr = csv.DictReader(open(args.positions))
+    positions = []
+    allocations = { target_type['Symbol']: {} for target_type in targets.itervalues() }
+    for pos_idx, pos in enumerate(positions_rdr):
+        if len(pos) == 0: continue
+        positions.append(pos)
+        position_type = symbol_map[pos['EquitySymbol']]
+        
+        if position_type == '':
+            print "WARNING. Position type for %s (%s) is an empty string!" % (pos['EquitySymbol'], pos['EquityDescription'])
+            position_type = "Other"
+
+        if not pos['EquitySymbol'] in allocations[position_type]:
+            allocations[position_type][pos['EquitySymbol']] = [pos]
+        else:
+            allocations[position_type][pos['EquitySymbol']].append(pos)
+    
+
+    allocation_totals = []
+    total_portfolio = 0
+    for asset_type, assets in allocations.iteritems():
+        asset_total = {'CAD':0, 'USD':0, 'total':0}
+        for asset_symbol, asset_positions in assets.iteritems():
+            for pos in asset_positions:
+                asset_total[pos['CurrencyDisplay']] += float(pos['MarketValue'])
+        
+        asset_total['total'] = asset_total['CAD'] + asset_total['USD'] * args.xchrate
+        total_portfolio += asset_total['total']
+
+        allocation_totals.append({
+            'type': asset_type,
+            'totals': asset_total
+        })
+
+    
+    print "Total portfolio value: %0.2f" % total_portfolio
+    print "Asset\tTarget\tCurrent\tMarket value"
+    for asset_totals in allocation_totals:
+        print "%s\t%0.2f\t%0.3f\t%0.2f" % (asset_totals["type"], float(targets[asset_totals["type"]]['Target']),
+                asset_totals["totals"]['total'] / total_portfolio, asset_totals["totals"]['total'])
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser(description='Show current portfolio balancing')
+    
+    parser.add_argument('--targets', type=str, default="targets.csv",
+                           help='targets csv file')
+    parser.add_argument('--symbolmap', type=str, default="symbolmap.csv",
+                           help='symbols map csv file')
+    parser.add_argument('--positions', type=str,
+                           help='positions csv file')
+    parser.add_argument('--xchrate', type=float,
+                           help='exchange rate (US to CAD)')
+
+    args = parser.parse_args()
+
+    print args
+    show_positions(args)
